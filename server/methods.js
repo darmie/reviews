47c9b3e8a111ac401897fe60b35f1af42776c8ca
'Damilare Darmie Akinlaja <dreakinlaja@gmail.com>
2013-02-21 19:38:00 +0100

    Update server/methods.js

--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,14 @@
 //method to post the reviews
 
-Meteor.methods({
-  review: function (name, a_review, title, imgsrc) {
-    if (a_review) {
-      if (!name) {
-        name = 'Anonymous';
-      }
-      Reviews.insert({username: name, story: a_review, title:title, imgsrc:imgsrc});
-      return true;
-    }
-    return false;
-  }
-});
+//Meteor.methods({
+  //review: function (name, a_review, title, imgsrc) {
+   // if (a_review) {
+     // if (!name) {
+      //  name = 'Anonymous';
+     // }
+     // Reviews.insert({username: name, story: a_review, title:title, imgsrc:imgsrc});
+     // return true;
+   // }
+   // return false;
+//  }
+//});
