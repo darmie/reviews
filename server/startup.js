dd42ad673ca9806ace4c5c2ff35582e7e1efbabe
'Damilare Darmie Akinlaja <dreakinlaja@gmail.com>
2013-02-21 19:53:06 +0100

    Update server/startup.js

--- server/startup.js
+++ server/startup.js
@@ -1,6 +1,7 @@
 // let's put something in our database if it's empty
 Meteor.startup(function () {
   if(Reviews.find().count() === 0) {
+    Reviews.insert({username: Session.get("username"), story: Session.get("story"), title: Session.get("title"), imgsrc: Session.get(imgsrc)});
     Reviews.insert({username: "darmie", story: "Cool movie", title:"null", imgsrc:"null"});
   }
 });
